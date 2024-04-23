610ffe0cfa46feb2af86b03db22ccb49090402c8
yrambe <ykrambe@gmail.com>
2024-04-24 05:47:32 +0700

    front end init

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <!-- <header>
+  </header> -->
+
+  <RouterView />
+</template>
---
+++ frontend/src/views/auth/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mt-16">
+    <h1 class="text-center text-4xl font-bold">Friends Diary</h1>
+    <img
+      src="@/assets/friendsdiary-logo.png"
+      alt="friendsdiary-logo"
+      width="500"
+      height="600"
+      class="mx-auto"
+    />
+    <h1 class="text-center text-4xl font-bold mt-3">Masuk</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
